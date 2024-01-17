badbf7398a7404dd1818bd6d2b37a69cb8ad3165
Hello World <152847625+pemanamgur@users.noreply.github.com>
2024-01-17 11:31:50 +0545

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todos.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Learn about redux toolkit</h1>
+    <AddTodo />
+    <Todos />
+    </> 
+  )
+}
+
+export default App
